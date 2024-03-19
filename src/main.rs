fdeecfa819495cdae9ee027e9a181785dd4e7d3e
kaushik raj <algorithmunloack@gmail.com>
2024-03-19 20:06:09 +0530

    The final changes hass been made

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 slint::include_modules!();
 
-const TAXPER: f64 = 0.30;
-const OWNERPER: f64 = 0.55;
-const PROFITPER: f64 = 0.05;
-const OPEXPER: f64 = 0.10;
+const LIKES: f64 = 0.30;
+const COMMENTS: f64 = 0.55;
+const REACH: f64 = 0.05;
+const  SHARE: f64 = 0.10;
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
@@ -12,11 +12,11 @@ fn main() -> Result<(), slint::PlatformError> {
     ui.on_divide_income(move |string| {
         let ui = ui_handle.unwrap();
         let num: f64 = string.trim().parse().unwrap();
-        let taxes: f64 = num * TAXPER;
-        let owner: f64 = num * OWNERPER;
-        let profit: f64 = num * PROFITPER;
-        let opex: f64 = num * OPEXPER;
-        let result = format!("Taxes: {:.2}\nOwner: {:.2}\nProfit: {:.2}\nOpEx: {:.2}", taxes, owner, profit, opex);
+        let likes: f64 = num * LIKES;
+        let comments: f64 = num * COMMENTS;
+        let reach: f64 = num * REACH;
+        let share: f64 = num * SHARE;
+        let result = format!("Likes: {:.2}\nComments: {:.2}\nReach: {:.2}\nShare: {:.2}", likes, comments, reach, share);
         ui.set_results(result.into());
     });
 
@@ -29,7 +29,7 @@ mod tests {
 
     #[test]
     fn equals_100() {
-        let result = TAXPER + OWNERPER + PROFITPER + OPEXPER;
+        let result =LIKES + COMMENTS + REACH + SHARE;
         let formatted = f64::trunc(result  * 100.0) / 100.0;
         assert_eq!(formatted, 1.00);
     }
